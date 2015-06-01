0ad4a6b8263bd4d4f7ebdf25921bacd2c8692eb8
ybykov <aS5bPr6n>
2015-06-01 22:53:47 +0200

    not yet done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,22 +1,45 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn [res base exp]
+                 (if (zero? exp)
+                   res
+                   (recur (* res base) base (dec exp))))]
+    (helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [ls a-seq]
+                 (if (empty? a-seq)
+                   ls
+                   (recur (first a-seq) (rest a-seq))))]
+    (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (let [helper (fn [seq1 seq2]
+                 (cond
+                  (not= (count seq1) (count seq2)) false
+                  (empty? seq1) true
+                  :else (recur (rest seq1) (rest seq2))))]
+    (helper seq1 seq2)))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [index 0
+         cur a-seq]
+    (cond
+     (empty? cur) nil
+     (pred (first cur)) index
+     :else (recur (inc index) (rest cur)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [sum 0
+         cnt 0
+         cur a-seq]
+    (if (empty? cur)
+      (/ sum cnt)
+      (recur (+ sum (first cur)) (inc cnt) (rest cur)))))
 
 (defn parity [a-seq]
-  ":(")
+  )
 
 (defn fast-fibo [n]
   ":(")
